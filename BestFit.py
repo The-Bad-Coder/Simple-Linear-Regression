66b9dc2fba2a9a77a9028eeb76310bba1e53e6c0
The-Bad-Coder <guptaom046@gmail.com>
2020-04-16 12:33:13 +0530

    Completed Program For BestFit line using two variables ......

--- BestFit.py
+++ BestFit.py
@@ -10,3 +10,15 @@ def SlopeIntercept(x,y):
     m = Sy /Sx
     c = meanY - m*meanX
 
+
+def plot():
+    global m , c , x , y 
+    plt.scatter(x , y , color = 'c', marker = 'd')
+    Y = np.array(m*x  + c)
+    plt.plot(x , Y , 'r:')
+    plt.show()
+
+x = np.array([1,2,3,4,5,6])
+y = np.array([3,4,2,4,5,7])
+SlopeIntercept(x,y)
+plot()
