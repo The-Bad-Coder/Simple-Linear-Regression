99028e11140a6e65cb8706162866b9d374ee298c
The-Bad-Coder <guptaom046@gmail.com>
2020-04-16 12:12:05 +0530

    Made Funcion for getting Slope and Intercept of the BestFit Line .....

--- BestFit.py
+++ BestFit.py
@@ -1,2 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
+def SlopeIntercept(x,y):
+    global meanX , meanY , m1 , c , m , c
+    meanX = np.mean(x)
+    meanY = np.mean(y)
+    Sy = np.sum( (x - meanX)*(y - meanY) )
+    Sx = np.sum( (x - meanX)**2 )
+    m = Sy /Sx
+    c = meanY - m*meanX
+
